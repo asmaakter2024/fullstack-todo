107d702a4809192e9474b5c1098aecdf18831e36
asmaakter2024 <tuly.asma@gmail.com>
2025-09-14 00:25:12 +0600

    primary setup

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,14 @@
-const express= require("express")
-const mongoose = require("mongoose")
+const express = require("express");
+const mongoose = require("mongoose");
 
-const app = express()
+const app = express();
+// const port = process.env.PORT ||  8000
+
+app.use(express.json());
+
+app.listen(8000, () => {
+  console.log("Server is running on port 8000");
+});
 
 
-app.listen(8000,()=>{
-  
-}
+
