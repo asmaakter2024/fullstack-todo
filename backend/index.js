188d5ccb57a7d14004628471be37a3a8ac5c44de
asmaakter2024 <tuly.asma@gmail.com>
2025-09-25 00:54:20 +0600

    check dotenv

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,15 @@ console.log("fullstack todo project part 1")
 
 const express = require("express");
 const mongoose = require("mongoose");
+const dotnv = require("dotenv")
 
+dotnv.config()
 const app = express();
 // const port = process.env.PORT || 8000;
 
 //.env ke console.log kore dekhte cacchi
 console.log(process.env.MONGO_URL)
+
 
 app.use(express.json());
 
