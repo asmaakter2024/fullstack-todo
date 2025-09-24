ffc9d141ec440fc7e5eee6ffc3b66b3813669d07
asmaakter2024 <tuly.asma@gmail.com>
2025-09-24 23:22:10 +0600

    .gitignore file create

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,14 @@
+console.log("fullstack todo project part 1")
+
 const express = require("express");
 const mongoose = require("mongoose");
 
 const app = express();
-// const port = process.env.PORT ||  8000
+// const port = process.env.PORT || 8000;
 
 app.use(express.json());
 
 app.listen(8000, () => {
   console.log("Server is running on port 8000");
 });
-
-
 
